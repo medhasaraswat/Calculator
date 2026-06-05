dc0582711c5e10651d8a43fc6adc243985c2b562
medhasaraswat <medhasaraswat17@gmail.com>
2026-06-05 23:34:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,65 @@
+function a(){
+document.f1.t1.value += document.f1.b1.value;
+}
+
+function b(){
+document.f1.t1.value += document.f1.b2.value;
+}
+
+function c(){
+document.f1.t1.value += document.f1.b3.value;
+}
+
+function d(){
+document.f1.t1.value += document.f1.b4.value;
+}
+
+function e(){
+document.f1.t1.value += document.f1.b5.value;
+}
+
+function f(){
+document.f1.t1.value += document.f1.b6.value;
+}
+
+function g(){
+document.f1.t1.value += document.f1.b7.value;
+}
+
+function h(){
+document.f1.t1.value += document.f1.b8.value;
+}
+
+function i(){
+document.f1.t1.value += document.f1.b9.value;
+}
+
+function j(){
+document.f1.t1.value += document.f1.b10.value;
+}
+
+function k(){
+document.f1.t1.value += document.f1.b11.value;
+}
+
+function l(){
+document.f1.t1.value += document.f1.b12.value;
+}
+
+function m(){
+document.f1.t1.value += document.f1.b13.value;
+}
+
+function o(){
+document.f1.t1.value += document.f1.b15.value;
+}
+
+
+
+function n(){
+document.f1.t1.value = eval(document.f1.t1.value);
+}
+
+function q(){
+document.f1.t1.value = "";
+}
